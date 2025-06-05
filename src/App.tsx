f368817dc0d2be364603be0372be0da2c908406b
muhamed1222 <134883387+muhamed1222@users.noreply.github.com>
2025-06-05 13:51:45 +0300

    fix: remove unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AppProvider } from './context/AppContext';
 import AppLayout from './components/Layout/AppLayout';
 import AuthLayout from './components/Auth/AuthLayout';
